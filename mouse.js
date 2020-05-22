fe29e37914efc62660e8f8e854876a01bf687570
liuhenggao <305180292@qq.com>
2020-05-23 01:40:54 +0800

    完成适配触摸屏幕的画板功能以及画笔的粗细，优化橡皮擦

--- mouse.js
+++ mouse.js
@@ -1,15 +1,22 @@
 let _moving = false
 let _wash = false
 let _fillall = false
+let _touch = false
 /** 获取浏览器视口的宽高 */
 let _width = document.documentElement.clientWidth
 let _height = document.documentElement.clientHeight
 
+/** 判断设备是否支持触摸 */
+let isTouch = 'ontouchstart' in document.body ? true : false
+
 /** 通过getElementById选取canvas元素 */
 let _canvas = document.getElementById('canvas')
 
-_canvas.width = _width - 120
-_canvas.height = _height - 40
+let _degree = 8
+let _range = _degree / 2
+
+_canvas.width = _width - 100
+_canvas.height = _height - 30
 
 let _contain = document.getElementById('container')
 _contain.style.width = '100vw'
@@ -18,6 +25,7 @@ _contain.style.height = '100vh'
 let _pencil = document.getElementById('pencil')
 let _eraser = document.getElementById('eraser')
 let _fill = document.getElementById('fill')
+
 _pencil.onclick = function (e) {
     _pencil.classList.add('active')
     _eraser.classList.remove('active')
@@ -43,7 +51,6 @@ _fill.onclick = function (e) {
 let _back = document.getElementById('back')
 let _clear = document.getElementById('clear')
 let _load = document.getElementById('load')
-
 
 
 let colorList = ["#000000", "#393D49", "#2F4056", "#75878a", "#d1d9e0", "#ffffff", "#1E9FFF", "#41555d", "#725e82", "#eacd76", "#a78e44", "#FFB800", "#FF5722", "#009688", "#5FB878"]
@@ -75,45 +82,67 @@ if (_canvas.getContext) {
     let _point = [0, 0]
     let _newPoint = [0, 0]
     let ctx = canvas.getContext('2d')
-    _canvas.onmousedown = function (e) {
-        if (_fillall) {
-            ctx.fillStyle = paintColor
-            ctx.fillRect(0, 0, _canvas.width, _canvas.height);
-        } else {
-            let pointX = e.layerX
-            let pointY = e.layerY
-            _point = [pointX, pointY]
-            ctx.fillStyle = paintColor
-            ctx.fillRect(pointX - 4, pointY - 4, 8, 8);
-            _moving = true
-            if (_wash) {
-                ctx.clearRect(pointX - 4, pointY - 4, 8, 8)
+    if (isTouch) {
+        _canvas.ontouchstart = function (e) {
+            if (_fillall) {
+                ctx.fillStyle = paintColor
+                ctx.fillRect(0, 0, _canvas.width, _canvas.height);
+            } else {
+                console.log(e)
+                let pointX = e.touches[0].clientX - 60
+                let pointY = e.touches[0].clientY - 20
+                _point = [pointX, pointY]
+                ctx.fillStyle = paintColor
+                ctx.fillRect(pointX - _range, pointY - _range, _degree, _degree);
+                _moving = true
+                if (_wash) {
+                    ctx.clearRect(pointX - _range, pointY - _range, _degree, _degree)
+                }
             }
         }
-
-    }
-    _canvas.onmouseleave = function (e) {
-        _moving = false
-    }
-
-    _canvas.onmousemove = function (e) {
-        let newPointX = e.layerX
-        let newPointY = e.layerY
-        if (_fillall) { return }
-        if (_moving) {
-            if (_wash) {
-                ctx.clearRect(newPointX - 5, newPointY - 5, 10, 10)
-            } else {
+        _canvas.ontouchmove = function (e) {
+            let newPointX = e.touches[0].clientX - 60
+            let newPointY = e.touches[0].clientY - 20
+            if (_fillall) { return }
+            if (_moving) {
                 _newPoint = [newPointX, newPointY]
                 drawLine(_point[0], _point[1], _newPoint[0], _newPoint[1])
                 _point = _newPoint
             }
-
         }
-    }
-
-    _canvas.onmouseup = function (e) {
-        _moving = false
+    } else {
+        _canvas.onmousedown = function (e) {
+            if (_fillall) {
+                ctx.fillStyle = paintColor
+                ctx.fillRect(0, 0, _canvas.width, _canvas.height);
+            } else {
+                let pointX = e.layerX
+                let pointY = e.layerY
+                _point = [pointX, pointY]
+                ctx.fillStyle = paintColor
+                ctx.fillRect(pointX - _range, pointY - _range, _degree, _degree);
+                _moving = true
+                if (_wash) {
+                    ctx.clearRect(pointX - _range, pointY - _range, _degree, _degree)
+                }
+            }
+        }
+        _canvas.onmouseleave = function (e) {
+            _moving = false
+        }
+        _canvas.onmousemove = function (e) {
+            let newPointX = e.layerX
+            let newPointY = e.layerY
+            if (_fillall) { return }
+            if (_moving) {
+                _newPoint = [newPointX, newPointY]
+                drawLine(_point[0], _point[1], _newPoint[0], _newPoint[1])
+                _point = _newPoint
+            }
+        }
+        _canvas.onmouseup = function (e) {
+            _moving = false
+        }
     }
 
     _clear.onclick = function (e) {
@@ -133,18 +162,34 @@ if (_canvas.getContext) {
     }
 
     function drawLine(x1, y1, x2, y2) {
-        console.log(paintColor)
         ctx.beginPath();
         ctx.moveTo(x1, y1);
-        ctx.lineWidth = 8
+        ctx.lineWidth = _degree
         ctx.lineTo(x2, y2);
-        ctx.strokeStyle = paintColor
         if (_wash) {
-
+            ctx.strokeStyle = '#fefefe'
         } else {
-
+            ctx.strokeStyle = paintColor
         }
         ctx.stroke();
         ctx.closePath()
     }
+}
+
+let _large = document.getElementById('large')
+let _medium = document.getElementById('medium')
+let _small = document.getElementById('small')
+let _mini = document.getElementById('mini')
+
+_large.onclick = function (e) {
+    _degree = 10
+}
+_medium.onclick = function (e) {
+    _degree = 8
+}
+_small.onclick = function (e) {
+    _degree = 6
+}
+_mini.onclick = function (e) {
+    _degree = 4
 }
